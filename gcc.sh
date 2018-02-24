ccbe5fe8142e8e50378f8af0981280eb478b58f4
Avneet Singh <30595749+AvyChanna@users.noreply.github.com>
2018-02-25 04:00:47 +0530

    Update gcc.sh

--- gcc.sh
+++ gcc.sh
@@ -1,6 +1,6 @@
 
 command -v gedit >/dev/null 2>&1 || { echo >&2 "Gedit is not installed.  Aborting."; exit 1; }
-command -v gcc >/dev/null 2>&1 || { echo >&2 "Gedit is not installed.  Aborting."; exit 1; }
+command -v gcc >/dev/null 2>&1 || { echo >&2 "GCC is not installed.  Aborting."; exit 1; }
 mkdir -p ~/.config/gedit/
 sudo chown $USER ~/.config/gedit/
 loda='#!/bin/bash
